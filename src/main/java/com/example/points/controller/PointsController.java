56a15fd135b5a27a6fe901f8fdf744c6c9e6ec80
ReginaCoelis <malenganeregina@gmail.com>
2020-11-18 19:27:28 -0600

    skeleton for deduct Points endpoints

--- src/main/java/com/example/points/controller/PointsController.java
+++ src/main/java/com/example/points/controller/PointsController.java
@@ -11,9 +11,15 @@ public class PointsController {
     public String pointBal(@PathVariable int userId){
         return " hello world" + userId;
     }
+    //pathVariable- for identifying a particular resource
     @PostMapping("/add/{userId}")
     public String addPoints(@PathVariable int userId, @RequestBody Transaction transaction){
         return "";
+    }
+    //RequestParameter
+    @PutMapping(value = "/deduct/{userId}", params = "points")
+    public String deductPoints(@PathVariable int userId, @RequestParam int points){
+        return userId+ " " + points;
     }
 
 }
